34d9ecb1f47731f99e2f7bb15f70f8195965d4d8
Stephen Kenny <Stephen@madmartigan.home>
2014-04-05 00:12:48 -0400

    created project

--- CCTableViewController.h
+++ CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Stephen Kenny on 4/4/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
